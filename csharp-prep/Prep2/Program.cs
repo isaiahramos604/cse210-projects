f1d66f6c5c81c4678a1dbffc7f4f828054ec519d
MixedIsh <isaiahramos604@byui.edu>
2023-09-16 13:18:15 -0500

    Homework

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,54 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your Grade ");
+        string valuefromUser = Console.ReadLine();
+
+        int x = int.Parse(valuefromUser);
+
+        int A = 90;
+        int B = 80;
+        int C = 70;
+        int D = 60;
+        int F = 59;
+
+        if (x >=A)
+        {
+            Console.WriteLine("You have an A");
+        }
+        else if (A >= x && x >= B)
+        {
+            Console.WriteLine("You have a B");
+            
+        }
+        else if (B>= x && x>= C)
+        {
+            Console.WriteLine ("You have a C");
+        }
+        else if (C>= x && x>= D)
+        {
+            Console.WriteLine("You have a D");
+        }
+        else if (F>= x)
+        {
+            Console.WriteLine("You have an F");
+        }
+        
+        if (x > F)
+        {
+            Console.WriteLine("Good Job Passing!");
+
+        }
+        else
+        {
+            Console.WriteLine("You got this next time!");
+        }
+    
+
+        
     }
 }

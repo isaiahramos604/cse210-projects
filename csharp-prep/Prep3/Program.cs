c6feb8115ddbe371d569b52d4dd2275e47048f66
MixedIsh <isaiahramos604@byui.edu>
2023-09-19 20:39:01 -0500

    hw

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,52 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+
+        int Magic = randomGenerator.Next(2,200);
+        int guess = -1;
+
+       while(guess != Magic)
+       {
+            Console.WriteLine("What is your guess for the Magic Number?");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > Magic)
+            {
+                Console.WriteLine("Lower");
+
+            }
+            else if (guess < Magic)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+           
+
+       }
+
+
+       
+        
+
+
+        
+
+
+
+
+
+
+
+        
+
+        
     }
 }

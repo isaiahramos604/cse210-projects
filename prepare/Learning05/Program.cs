a51625f39e769699c29c266f7bf40d2aa6efa7cd
MixedIsh <isaiahramos604@byui.edu>
2023-12-02 18:41:43 -0600

    Hw

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square1 = new Square("blue", 5);
+      
+
+        Rectangle rectangle1 = new Rectangle("red", 6, 5);
+        
+
+        Circle circle1 = new Circle("Orange",5);
+        
+        
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(circle1);
+        shapes.Add(rectangle1);
+        shapes.Add(square1);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+
+            double area = shape.GetArea();
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+
+        }
+
+        
+
+
+
+        
     }
 }

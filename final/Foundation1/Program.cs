f5d99b067f2fa6722e0831c8569a64d1bf2efbb1
MixedIsh <isaiahramos604@byui.edu>
2023-12-08 22:22:50 -0600

    final pt 1

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,9 +1,134 @@
 using System;
 
+class Comment
+{
+    protected string _commenterName;
+    private string _commentText;
+    public string GetName()
+    {
+        return _commenterName;
+    }
+    public void SetName(string commenterName)
+    {
+        _commenterName = commenterName;
+    }
+    public string GetCommenterText()
+    {
+        return _commentText;
+    }
+    public void SetCommenterText(string commenterText)
+    {
+        _commentText = commenterText;
+    }
+    public Comment(string commenterName, string commenterText)
+    {
+        _commenterName = commenterName;
+        _commentText = commenterText;
+    }
+
+}
+
+public class Video
+{
+    private string _titlemp4;
+    private string _authormp4;
+    private int _lengthinSeconds;
+    public string GetTitle()
+    {
+        return _titlemp4;
+    }
+    public void SetTitle(string title)
+    {
+        _titlemp4 = title;
+    }
+    public string GetAuthor()
+    {
+        return _authormp4;
+    }
+    public void SetAuthor(string author)
+    {
+        _authormp4 = author;
+    }
+    public int GetLength()
+    {
+        return _lengthinSeconds;
+    }
+    public void SetLength(int length)
+    {
+        _lengthinSeconds = length;
+    }
+    private List<Comment> comments;
+    public Video(string title, string author, int length)
+    {
+        _authormp4= author;
+        _titlemp4 = title;
+        _lengthinSeconds = length;
+        comments = new List<Comment>();
+        
+    }
+
+    public void AddComment(string commenterName, string commenterText)
+    {
+        Comment newComment = new Comment(commenterName, commenterText);
+        comments.Add(newComment);
+    }
+
+    public int GetNumberOfComments()
+    {
+        return comments.Count;
+    }
+
+    public void DisplayVideoDetails()
+    {
+        Console.WriteLine($"Title: {_titlemp4}");
+        Console.WriteLine($"Author {_authormp4}");
+        Console.WriteLine($"Length: {_lengthinSeconds} seconds");
+        Console.WriteLine($"Number of comments: {GetNumberOfComments()}");
+        Console.WriteLine("Comments:");
+        foreach(var comment in comments)
+        {
+            Console.WriteLine($"- {comment.GetName()}: {comment.GetCommenterText()}");
+    }
+
+    Console.WriteLine();
+}
+}
+
+
+
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        Video myVidoe = new Video("Making popcorn into a hotdog", "HotdogMan56", 30);
+        myVidoe.AddComment("User 1", "Crazy Video");
+        myVidoe.AddComment("User 2", "Thanks for Vid");
+        myVidoe.AddComment("User 3", "Rated 5/5");
+        myVidoe.DisplayVideoDetails();
+        
+        Console.WriteLine("Press Enter to show the next video");
+        Console.ReadLine();
+        Console.Clear();
+
+        Video myVideo2 = new Video("Skate Board Trick", "JaneSkates", 40);
+        myVideo2.AddComment("User 1", "Nice Video");
+        myVideo2.AddComment("User 2", "Cool Trick");
+        myVideo2.AddComment("User 3", "It was meh");
+        myVideo2.DisplayVideoDetails();
+        Console.WriteLine("Press Enter to show the next video");
+        Console.ReadLine();
+        Console.Clear();
+
+
+        Video myVideo3 = new Video("Quick Clothing Show", "BobBillysClothes", 50);
+        myVideo3.AddComment("User 1", "Nice Hat");
+        myVideo3.AddComment("User 2", "Cool clothing style");
+        myVideo3.AddComment("User 3", "Its to baggy in my opinion");
+        myVideo3.DisplayVideoDetails();
+        Console.WriteLine("End of video list, press enter to exit");
+        Console.ReadLine();
+        Console.Clear();
+
     }
 }
--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -1,9 +1,245 @@
 using System;
+using System.Collections.Generic;
+
+public class Address
+{
+    private string _streetAddress;
+    private string _city;
+    private string _state;
+    private string _country;
+
+    public Address(string country, string state, string city, string streetAddress)
+    {
+        _country = country;
+        _state = state;
+        _city = city;
+        _streetAddress = streetAddress;
+    }
+
+    public bool IsInUSA()
+    {
+        return _country.Equals("USA", StringComparison.OrdinalIgnoreCase);
+    }
+
+    public override string ToString()
+    {
+        return $"{_streetAddress}, {_city}, {_state}, {_country}";
+    }
+}
+
+class Product
+{
+    private string _name;
+    private int _productID;
+    private decimal _price;
+    private int _quantity;
+
+    public Product(string name, int productID, decimal price, int quantity)
+    {
+        _name = name;
+        _productID = productID;
+        _price = price;
+        _quantity = quantity;
+    }
+
+    public decimal GetTotalPrice()
+    {
+        return _price * _quantity;
+    }
+
+    public string GetNameOfProduct()
+    {
+        return _name;
+    }
+
+    public int GetProductID()
+    {
+        return _productID;
+    }
+
+    public decimal GetProductPrice()
+    {
+        return _price;
+    }
+
+    public int GetQuantity()
+    {
+        return _quantity;
+    }
+
+    public override string ToString()
+    {
+        return $"Name: {_name}, ID: {_productID}";
+    }
+}
+
+class Customer
+{
+    private string _customerName;
+    private Address _address;
+
+    public Customer(string customerName, Address address)
+    {
+        _customerName = customerName;
+        _address = address;
+    }
+
+    public Address GetCountry()
+    {
+        return _address;
+    }
+
+    public override string ToString()
+    {
+        return $"{_customerName}, {_address}";
+    }
+}
+
+class Order
+{
+    private List<Product> _products;
+    private Customer _customer;
+
+    public Order(Customer customer)
+    {
+        _products = new List<Product>();
+        _customer = customer;
+    }
+
+    public void AddProduct(Product product)
+    {
+        _products.Add(product);
+    }
+
+    public decimal CalculateTotalCost()
+    {
+        decimal totalCost = 0;
+
+        foreach (Product product in _products)
+        {
+            totalCost += product.GetTotalPrice();
+        }
+
+        if (_customer.GetCountry().IsInUSA())
+        {
+            totalCost += 5;
+        }
+        else
+        {
+            totalCost += 35;
+        }
+
+        return totalCost;
+    }
+
+    public string GetPackingLabel()
+    {
+        string packingLabel = "Packing Label:\n";
+
+        foreach (Product product in _products)
+        {
+            packingLabel += $"{product.ToString()}\n";
+        }
+
+        return packingLabel;
+    }
+
+    public string GetShippingLabel()
+    {
+        return $"Shipping Label:\n{_customer.ToString()}";
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        Console.WriteLine("Welcome to the Shopping App!");
+
+        Console.WriteLine("Please enter your name:");
+        string customerName = Console.ReadLine();
+
+        Console.WriteLine("Please enter your street address:");
+        string streetAddress = Console.ReadLine();
+
+        Console.WriteLine("Please enter your city:");
+        string city = Console.ReadLine();
+
+        Console.WriteLine("Please enter your state/province:");
+        string state = Console.ReadLine();
+
+        Console.WriteLine("Please enter your country:");
+        string country = Console.ReadLine();
+
+        Address customerAddress = new Address(country, state, city, streetAddress);
+        Customer customer = new Customer(customerName, customerAddress);
+        Order order = new Order(customer);
+        List<Product> availableProducts = new List<Product>();
+
+        
+        availableProducts.Add(new Product("Bananas", 1, 10.0m, 100));
+        availableProducts.Add(new Product("Oranges", 2, 20.0m, 50));
+        availableProducts.Add(new Product("Apples", 3, 15.0m, 75));
+
+        bool shopping = true;
+
+        while (shopping)
+        {
+            Console.WriteLine("\nAvailable Products:");
+            Console.WriteLine("-------------------");
+            foreach (Product product in availableProducts)
+            {
+                Console.WriteLine($"{product.GetProductID()}. {product.GetNameOfProduct()} - Price: {product.GetProductPrice():C}");
+            }
+
+            Console.WriteLine("\nEnter the ID of the product you want to add to the basket (0 to checkout):");
+            if (int.TryParse(Console.ReadLine(), out int selectedProductId))
+            {
+                if (selectedProductId == 0)
+                {
+                    shopping = false;
+                }
+                else
+                {
+                    Product selectedProduct = availableProducts.Find(p => p.GetProductID() == selectedProductId);
+                    if (selectedProduct != null)
+                    {
+                        Console.WriteLine("Enter the quantity:");
+                        if (int.TryParse(Console.ReadLine(), out int quantity))
+                        {
+                            if (quantity > 0 && quantity <= selectedProduct.GetQuantity())
+                            {
+                                Product productToAdd = new Product(selectedProduct.GetNameOfProduct(), selectedProduct.GetProductID(), selectedProduct.GetProductPrice(), quantity);
+                                order.AddProduct(productToAdd);
+                                Console.WriteLine($"Added {quantity} {selectedProduct.GetNameOfProduct()} to the basket.");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Invalid quantity or insufficient stock.");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input for quantity.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid product ID.");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid product ID.");
+            }
+
+            Console.WriteLine("\n-----------------------------------------");
+        }
+
+        Console.WriteLine("\nOrder Summary:");
+        Console.WriteLine(order.GetPackingLabel());
+        Console.WriteLine(order.GetShippingLabel());
+        Console.WriteLine($"Total Cost: {order.CalculateTotalCost():C}");
     }
 }
